625ddfac3bf2a4df42aa316230a811f21f6f2efe
shalomsantos <shalomsantos1234@gmail.br>
2026-04-15 09:25:35 -0300

    feat: readme updated

---
+++ README.md
@@ -0,0 +1,61 @@
+## Passo a passo para rodar o projeto
+Clone o projeto e depois acesse a pasta
+```sh
+cd myAplicationApp/
+```
+
+
+Crie o Arquivo .env
+```sh
+cp .env.example .env
+```
+
+
+Atualize essas variáveis de ambiente no arquivo .env
+```dosini
+APP_NAME="myAplicationApp"
+APP_URL=http://localhost
+
+DB_CONNECTION=mysql
+DB_HOST=mysql
+DB_PORT=mysql
+DB_DATABASE=mysql
+DB_USERNAME=mysql
+DB_PASSWORD=mysql
+
+CACHE_DRIVER=redis
+QUEUE_CONNECTION=redis
+SESSION_DRIVER=redis
+
+REDIS_HOST=redis
+REDIS_PASSWORD=null
+REDIS_PORT=6379
+```
+
+
+Suba os containers do projeto
+```sh
+docker-compose up -d
+```
+
+
+Acesse o container
+```sh
+docker-compose exec my_aplication_php bash
+```
+
+
+Instale as dependências do projeto
+```sh
+composer install
+```
+
+
+Gere a key do projeto Laravel
+```sh
+php artisan key:generate
+```
+
+
+Acesse o projeto
+[http://localhost](http://localhost)
---
+++ aplication/resources/js/Pages/Crud/basezero/EditeBzero.vue
@@ -0,0 +1,223 @@
+<template>
+    <DefaultLayout
+        v-model="viewOption"
+        title="Bases Lista"
+        :location="location"
+    >
+        <v-row dense>
+            <v-col cols="12" class="d-flex justify-end ga-3 mb-1">
+                <v-btn
+                    class="text-none"
+                    prepend-icon="mdi-filter"
+                    color="green-darken-1"
+                    text="Filtro"
+                    @click="dialogFilter = true"
+                />
+                <v-btn
+                    class="text-none"
+                    prepend-icon="mdi-plus"
+                    color="green-darken-1"
+                    text="Nova base"
+                    @click="dialogNewBasezero = true"
+                />
+            </v-col>
+            <v-col
+                cols="6"
+                v-if="dados.data.length > 0 && viewOption"
+                v-for="(item, id) in dados.data"
+                :key="id"
+            >
+                <v-hover>
+                    <template v-slot:default="{ isHovering, props }">
+                        <v-card
+                            v-bind="props"
+                            :color="isHovering ? 'teal-lighten-5' : undefined"
+                        >
+                            <template v-slot:title>
+                                <v-btn
+                                    variant="outlined"
+                                    color="green-darken-1"
+                                >
+                                    <p class="text-h6 text-green-darken-1">
+                                        #{{ item.id }}
+                                    </p>
+                                    <v-icon
+                                        icon="mdi-text-box"
+                                        color="green-darken-1"
+                                    ></v-icon>
+                                </v-btn>
+                                {{ item.projeto.nome }}
+                            </template>
+                            <template v-slot:item>
+                                <v-row no-gutters class="pt-2">
+                                    <v-col cols="3">
+                                        <p class="text-body-2">Status</p>
+                                        <div>
+                                            <p
+                                                class="text-body-2 text-disabled"
+                                            >
+                                                {{ item.status.nome }}
+                                            </p>
+                                        </div>
+                                    </v-col>
+                                    <v-col cols="3">
+                                        <p class="text-body-2">Ano</p>
+                                        <div>
+                                            <p
+                                                class="text-body-2 text-disabled"
+                                            >
+                                                {{ item.ano }}
+                                            </p>
+                                        </div>
+                                    </v-col>
+                                    <v-col cols="3">
+                                        <p class="text-body-2">Criado em</p>
+                                        <div>
+                                            <p
+                                                class="text-body-2 text-disabled"
+                                            >
+                                                {{ isDate(item.created_at) }}
+                                            </p>
+                                        </div>
+                                    </v-col>
+                                    <v-col cols="3">
+                                        <p class="text-body-2">Por</p>
+                                        <div>
+                                            <p
+                                                class="text-body-2 text-disabled"
+                                            >
+                                                {{ item.created_by.name }}
+                                            </p>
+                                        </div>
+                                    </v-col>
+
+                                    <v-sheet
+                                        class="d-flex flex-wrap ga-2 bg-transparent pt-3"
+                                    >
+                                        <v-chip
+                                            size="x-small"
+                                            color="green"
+                                            variant="flat"
+                                            v-for="plataforma in item.plataformas"
+                                            :key="plataforma.id"
+                                        >
+                                            {{ plataforma.nome }}
+                                        </v-chip>
+                                    </v-sheet>
+                                </v-row>
+                            </template>
+                        </v-card>
+                    </template>
+                </v-hover>
+            </v-col>
+            <v-col cols="12" v-else-if="dados.data.length > 0 && !viewOption">
+                <v-table
+                    density="compact"
+                    class="bg-green-lighten-5"
+                    striped="even"
+                >
+                    <thead>
+                        <tr>
+                            <th class="text-left">Id</th>
+                            <th class="text-left">Projeto</th>
+                            <th class="text-left">Status</th>
+                            <th class="text-left">Ano</th>
+                            <th class="text-left">Criado em</th>
+                            <th class="text-left">Criador por</th>
+                            <th class="text-left">***</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item, id) in dados.data" :key="id">
+                            <td>{{ item.id }}</td>
+                            <td>{{ item.projeto.nome }}</td>
+                            <td>{{ item.status.nome }}</td>
+                            <td>{{ item.ano }}</td>
+                            <td>{{ isDate(item.created_at) }}</td>
+                            <td>
+                                <v-chip
+                                    size="x-small"
+                                    color="green"
+                                    variant="flat"
+                                >
+                                    {{ item.created_by.name }}
+                                </v-chip>
+                            </td>
+                            <td>
+                                <v-btn
+                                    class="text-none me-1"
+                                    icon="mdi-delete"
+                                    density="compact"
+                                    color="red-lighten-2"
+                                ></v-btn>
+                            </td>
+                        </tr>
+                    </tbody>
+                </v-table>
+            </v-col>
+            <v-col cols="12" v-else>
+                <EmptyData />
+            </v-col>
+            <v-col cols="12" class="d-flex justify-center">
+                <v-pagination
+                    v-model="dados.current_page"
+                    :length="dados.last_page"
+                    :total-visible="4"
+                    @update:model-value="updatePage"
+                    active-color="green-darken-4"
+                    color="green-lighten-1"
+                    class="position-absolute bottom-0 mb-3"
+                    style="left: 50%; transform: translateX(-50%); z-index: 15"
+                    density="comfortable"
+                    variant="flat"
+                ></v-pagination>
+            </v-col>
+        </v-row>
+
+        <!-- Dialogs -->
+        <FiltroBase v-model="dialogFilter" @onFilter="filtrarBases" />
+
+        <NovaBase
+            v-model="dialogNewBasezero"
+            :projetos="props.projetos"
+            @onCloseDialog="dialogNewBasezero = false"
+        />
+        <!-- Feedback -->
+        <NormalFeedback v-model="feedback" />
+    </DefaultLayout>
+</template>
+
+<script setup>
+import NormalFeedback from "@/Components/Feedback/NormalFeedback.vue";
+import FiltroBase from "@/Components/Dialogs/Bzero/FiltroBase.vue";
+import NovaBase from "@/Components/Dialogs/Bzero/NovaBase.vue";
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+import EmptyData from "@/Components/EmptyData.vue";
+import { router } from "@inertiajs/vue3";
+import { ref } from "vue";
+import axios from "axios";
+
+const props = defineProps({
+    bzeros: Object,
+    projetos: Object,
+    preferencias: Object,
+});
+
+const location = [
+    { title: "Kronos", disabled: false, href: "/" },
+    { title: "Lista", disabled: true },
+];
+
+const viewOption = ref(props.preferencias?.listagem_menu ?? 0);
+const dados = ref(props.bzeros);
+
+// Feedback
+const feedback = ref({
+    show: false,
+    timeout: 2000,
+    color: "success",
+    text: "",
+});
+</script>
+
+<style scoped></style>
